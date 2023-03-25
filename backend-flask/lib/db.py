d7a3c29ef0ed678ed206bc4794d28f1eb8db9364
fptycl <zilenk@gmail.com>
2023-03-25 07:29:09 +0000

    implement dynamodb into our application

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -5,7 +5,6 @@ import sys
 from flask import current_app as app
 
 class Db:
-
   def __init__(self):
     self.init_pool()
 
@@ -24,11 +23,11 @@ class Db:
       template_content = f.read()
     return template_content
 
-
   def init_pool(self):
     connection_url = os.getenv("CONNECTION_URL")
-    self.pool = ConnectionPool(connection_url) 
-
+    self.pool = ConnectionPool(connection_url)
+  # we want to commit data such as an insert
+  # be sure to check for RETURNING in all uppercases
   def print_params(self,params):
     blue = '\033[94m'
     no_color = '\033[0m'
@@ -40,48 +39,49 @@ class Db:
     cyan = '\033[96m'
     no_color = '\033[0m'
     print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
-    print(sql, params)
-
-  def query_commit(self, sql, params={}):
+    print(sql,params)
+  def query_commit(self,sql,params={}):
     self.print_sql('commit with returning',sql,params)
+
     pattern = r"\bRETURNING\b"
     is_returning_id = re.search(pattern, sql)
-    
+
     try:
-        with self.pool.connection() as conn:
-          cur = conn.cursor()
-          cur.execute(sql, params)
-          if is_returning_id:
-            returning_id = cur.fetchone()[0]
-          conn.commit() 
-          if is_returning_id:
-            return returning_id
+      with self.pool.connection() as conn:
+        cur =  conn.cursor()
+        cur.execute(sql,params)
+        if is_returning_id:
+          returning_id = cur.fetchone()[0]
+        conn.commit() 
+        if is_returning_id:
+          return returning_id
     except Exception as err:
       self.print_sql_err(err)
-
-  def query_array_json(self, sql, params={}):
+  # when we want to return a json object
+  def query_array_json(self,sql,params={}):
     self.print_sql('array',sql,params)
-    
+
     wrapped_sql = self.query_wrap_array(sql)
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
-        cur.execute(wrapped_sql, params)
+        cur.execute(wrapped_sql,params)
         json = cur.fetchone()
         return json[0]
+  # When we want to return an array of json objects
+  def query_object_json(self,sql,params={}):
 
-  def query_object_json(self, sql, params={}):
     self.print_sql('json',sql,params)
     self.print_params(params)
     wrapped_sql = self.query_wrap_object(sql)
+
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
-        cur.execute(wrapped_sql, params)
+        cur.execute(wrapped_sql,params)
         json = cur.fetchone()
         if json == None:
           "{}"
         else:
           return json[0]
-  
   def query_value(self,sql,params={}):
     self.print_sql('value',sql,params)
     with self.pool.connection() as conn:
@@ -89,38 +89,34 @@ class Db:
         cur.execute(sql,params)
         json = cur.fetchone()
         return json[0]
-          
-  def query_wrap_object(self, template):
+  def query_wrap_object(self,template):
     sql = f"""
     (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
     {template}
     ) object_row);
     """
     return sql
-
-  def query_wrap_array(self, template):
+  def query_wrap_array(self,template):
     sql = f"""
     (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
     {template}
     ) array_row);
     """
     return sql
+  def print_sql_err(self,err):
+    # get details about the exception
+    err_type, err_obj, traceback = sys.exc_info()
 
-  def print_sql_err(self, err):
+    # get the line number when exception occured
+    line_num = traceback.tb_lineno
 
-      # get details about the exception
-      err_type, err_obj, traceback = sys.exc_info()
+    # print the connect() error
+    print ("\npsycopg ERROR:", err, "on line number:", line_num)
+    print ("psycopg traceback:", traceback, "-- type:", err_type)
 
-      # get the line number when exception occured
-      line_num = traceback.tb_lineno
+    # print the pgcode and pgerror exceptions
+    print ("pgerror:", err.pgerror)
+    print ("pgcode:", err.pgcode, "\n")
 
-      # print the connect() error
-      print ("\npsycopg ERROR:", err, "on line number:", line_num)
-      print ("psycopg traceback:", traceback, "-- type:", err_type)
-
-      # print the pgcode and pgerror exceptions
-      print ("pgerror:", err.pgerror)
-      print ("pgcode:", err.pgcode, "\n")
-
-db = Db() 
+db = Db()
 
