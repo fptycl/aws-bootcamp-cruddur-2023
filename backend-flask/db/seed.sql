be2ad7c5bdcd9cec7b9d29b899866882e0be3565
fptycl <zilenk@gmail.com>
2023-03-30 01:41:25 +0000

    Add own name handle to display in homw

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,13 +2,15 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
+  ('Chee Leng', 'z_lenk@yahoo.com.sg', 'CheeLeng', 'MOCK'),
   ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
   ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
+
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
-    'This was imported as seed data!',
+    (SELECT uuid from public.users WHERE users.handle = 'CheeLeng' LIMIT 1),
+    'This was imported as seed data for Chee Leng!',
     current_timestamp + interval '10 day'
   )
