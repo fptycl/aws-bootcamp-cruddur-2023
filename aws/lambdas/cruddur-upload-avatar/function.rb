41cf4902fd489b27c58844dbdea027b321578415
fptycl <zilenk@gmail.com>
2023-04-25 13:41:07 +0000

    upload avatar wip

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -10,7 +10,7 @@ def handler(event:, context:)
     { 
       headers: {
         "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": "https://3000-omenking-awsbootcampcru-2n1d6e0bd1f.ws-us94.gitpod.io",
+        "Access-Control-Allow-Origin": "https://3000-fptycl-awsbootcampcrudd-mryi3t115m0.ws-us95.gitpod.io",
         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
       },
       statusCode: 200
@@ -38,7 +38,7 @@ def handler(event:, context:)
     { 
       headers: {
         "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": "https://3000-omenking-awsbootcampcru-2n1d6e0bd1f.ws-us94.gitpod.io",
+        "Access-Control-Allow-Origin": "https://3000-fptycl-awsbootcampcrudd-mryi3t115m0.ws-us95.gitpod.io",
         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
       },
       statusCode: 200, 
