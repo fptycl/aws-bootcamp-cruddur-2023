5f32f3cb3d2c384e43e583cdc04cc45d70abf080
fptycl <zilenk@gmail.com>
2023-06-06 13:11:36 +0000

    refactor error handling and fetch requests

--- frontend-react-js/src/pages/SignupPage.js
+++ frontend-react-js/src/pages/SignupPage.js
@@ -2,9 +2,8 @@ import './SignupPage.css';
 import React from "react";
 import {ReactComponent as Logo} from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
+import FormErrors from 'components/FormErrors';
 
-// [TODO] Authenication
-// import Cookies from 'js-cookie'
 import { Auth } from 'aws-amplify';
 
 export default function SignupPage() {
@@ -19,14 +18,17 @@ export default function SignupPage() {
   const onsubmit = async (event) => {
     event.preventDefault();
     setErrors('')
+    console.log('username',username)
+    console.log('email',email)
+    console.log('name',name)
     try {
       const { user } = await Auth.signUp({
         username: email,
         password: password,
         attributes: {
-            name: name,
-            email: email,
-            preferred_username: username,
+          name: name,
+          email: email,
+          preferred_username: username,
         },
         autoSignIn: { // optional - enables auto sign in after user is confirmed
           enabled: true,
@@ -52,11 +54,6 @@ export default function SignupPage() {
   }
   const password_onchange = (event) => {
     setPassword(event.target.value);
-  }
-
-  let el_errors;
-  if (errors){
-    el_errors = <div className='errors'>{errors}</div>;
   }
 
   return (
@@ -107,7 +104,7 @@ export default function SignupPage() {
               />
             </div>
           </div>
-          {el_errors}
+          <FormErrors errors={errors} />
           <div className='submit'>
             <button type='submit'>Sign Up</button>
           </div>
